ead163b38eee253dee51f56cc85c672969f87424
takaya <sample@email.com>
2024-11-19 20:33:33 +0900

    migrationをdocker-compose buildで作成できるように修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
+# サーバーPIDファイルの削除（Railsが再起動できるようにする）
 rm -f /myapp/tmp/pids/server.pid
+
+# データベースの作成とマイグレーション
+bundle exec rails db:create || true
+bundle exec rails db:migrate || true
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
